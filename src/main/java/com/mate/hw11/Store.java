23725684c9a10863140d6824004d4c17e398e1b6
shcherbynaa <47137576+shcherbynaa@users.noreply.github.com>
2019-04-02 17:57:18 +0200

    1 commit in new branch

---
+++ src/main/java/com/mate/hw11/Store.java
@@ -0,0 +1,131 @@
+package com.mate.hw11;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.type.TypeReference;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Store {
+    private static final Logger LOGGER = Logger.getLogger(Store.class);
+    private List<Fruit> storeDatabase;
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private static List<Fruit> fruits;
+    private static final String PATH = "delivery.json";
+
+    public Store() {
+        storeDatabase = new ArrayList<>();
+    }
+
+    public List<Fruit> getStoreDatabase() {
+        return storeDatabase;
+    }
+
+    public void addFruits(String pathToJsonFile) {
+        List<Fruit> delivery;
+        try (FileInputStream inputStream = new FileInputStream(pathToJsonFile)) {
+            delivery = mapper.readValue(inputStream, new TypeReference<List<Fruit>>() {
+            });
+            storeDatabase.addAll(delivery);
+            delivery.forEach(del -> System.out.println(del.getFruitType().toString()));
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    public void save(String pathToJsonFile) {
+        try (FileOutputStream outputStream = new FileOutputStream(pathToJsonFile)) {
+            mapper.writeValue(outputStream, storeDatabase);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    public void load(String pathToJsonFile) {
+        storeDatabase.clear();
+        try (FileInputStream inputStream = new FileInputStream(pathToJsonFile)) {
+            storeDatabase = mapper.readValue(inputStream, new TypeReference<List<Fruit>>() {
+            });
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+
+    public List<Fruit> getSpoiledFruits(LocalDate date) {
+        List<Fruit> listSpoiledFruits = new ArrayList<>();
+        storeDatabase.forEach(fruit -> {
+            LocalDate spoiledAt = fruit.getDateOfDelivery().plusDays(fruit.getExpirationDate());
+            System.out.println(spoiledAt.toString());
+            if (date.isAfter(spoiledAt)) {
+                listSpoiledFruits.add(fruit);
+            }
+        });
+        listSpoiledFruits.forEach(fruit -> System.out.println(fruit));
+        return listSpoiledFruits;
+    }
+
+    public List<Fruit> getSpoiledFruits(LocalDate date, FruitType type) {
+        List<Fruit> listSpoiledFruits = new ArrayList<>();
+
+        storeDatabase.forEach(fruit -> {
+            LocalDate spoiledAt = fruit.getDateOfDelivery().plusDays(fruit.getExpirationDate());
+            System.out.println(spoiledAt.toString());
+            if (date.isAfter(spoiledAt) && (fruit.getFruitType() == type)) {
+                listSpoiledFruits.add(fruit);
+            }
+        });
+
+        listSpoiledFruits.forEach(fruit -> System.out.println(fruit));
+        return listSpoiledFruits;
+    }
+
+
+    public List<Fruit> getAvailableFruits(LocalDate date) {
+        List<Fruit> listAvailableFruits = new ArrayList<>();
+        storeDatabase.forEach(fruit -> {
+            LocalDate spoiledAt = fruit.getDateOfDelivery().plusDays(fruit.getExpirationDate());
+            if (date.isBefore(spoiledAt)) {
+                listAvailableFruits.add(fruit);
+            }
+        });
+        return listAvailableFruits;
+    }
+
+    public List<Fruit> getAvailableFruits(LocalDate date, FruitType type) {
+        List<Fruit> listAvailableFruits = new ArrayList<>();
+        storeDatabase.forEach(fruit -> {
+            LocalDate spoiledAt = fruit.getDateOfDelivery().plusDays(fruit.getExpirationDate());
+            if (date.isBefore(spoiledAt) && (fruit.getFruitType() == type)) {
+                listAvailableFruits.add(fruit);
+            }
+        });
+        return listAvailableFruits;
+    }
+
+    public List<Fruit> getAddedFruits(LocalDate date) {
+        fruits = new ArrayList<>();
+        storeDatabase.forEach(fruit -> {
+            if (fruit.getDateOfDelivery().equals(date)){
+                fruits.add(fruit);
+            }
+        });
+        return fruits;
+    }
+
+    public List<Fruit> getAddedFruits(LocalDate date,  FruitType type) {
+        fruits = new ArrayList<>();
+        storeDatabase.forEach(fruit -> {
+            if (fruit.getDateOfDelivery().equals(date) && (fruit.getFruitType().equals(type))){
+                fruits.add(fruit);
+            }
+        });
+        return fruits;
+    }
+}
